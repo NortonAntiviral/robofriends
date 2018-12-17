3c936713da6b68aa66e22c24071a2a4a53c7bdb2
NortonAntiviral <officialantiviral@gmail.com>
2018-12-16 21:17:04 -0500

    added mutliple cards with avatars and info

---
+++ src/Robots.js
@@ -0,0 +1,37 @@
+export const Robots = [
+  {id:1,
+  name: 'Max Beauvueare',
+  username: 'El Cheapo',
+  email:'ElCheapo@gmail.com'
+},
+{id:2,
+name: 'Christine Bama',
+username:'Pogohontas',
+email:'Pogohonatas@live.com'
+},
+{id:3,
+name:'Remy Leboux',
+username: 'Benzy',
+email:'BenzinoThaGod@gmail.com'
+},
+{id:4,
+name: 'Rory Noximus',
+username:'Nox',
+email: 'Noximus@gmail.com'
+},
+{id:5,
+name: 'Joe Garcia',
+username: 'CodingPhase',
+email:'TheCodingWay@CodingPhase.com'
+},
+{id:6,
+name: 'Lisa Ramos',
+username: 'Lee',
+email:'LeeRa@gmail.com'
+},
+{id:7,
+name:'Zoey Crymes',
+username:'Killa B',
+email:'BeezyCheesy@gmail.com'
+}
+]
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+// import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { Robots } from './Robots';
 
-ReactDOM.render(<Hello greeting={'Hello' + ' React Ninja'}/>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+  <CardList Robots = {Robots}/>
+  , document.getElementById('root'));
 serviceWorker.unregister();
