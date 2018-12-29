f7b020604e63533f881b0e33b5573f6505b45382
NortonAntiviral <officialantiviral@gmail.com>
2018-12-29 01:55:04 -0500

    made search bar scrollable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React,{ Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { Robots } from './Robots';
+// import { Robots } from './Robots';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -12,11 +13,14 @@ class App extends Component {
       Robots: [],
       searchfield: ''
     }
-    console.log('1');
+
   }
 
   componentDidMount() {
-  this.setState({Robots:Robots});
+    fetch('https://jsonplaceholder.typicode.com/users').then(response=> response.json())
+    .then(users =>
+      this.setState({Robots: users}));
+
   }
 
   onSearchChange = (event) => {
@@ -29,13 +33,21 @@ class App extends Component {
     const filteredRobots = this.state.Robots.filter(Robots => {
       return Robots.name.toLowerCase().includes(this.searchfield.toLowerCase());
     } )
+
+    if (this.state.robots.length === 0){
+      return <h1>Loading</h1>
+    } else {
     return(
       <div className='tc'>
       <h1 className='f2'>RoboFriends</h1>
       <SearchBox  SearchChange={this.onSearchChange}/>
+      <Scroll>
       <CardList Robots={filteredRobots}/>
-      </div>
+    </Scroll>
+  </div>
+
     );
+  }
   }
 }
 
